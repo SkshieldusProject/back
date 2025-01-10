6a75d57d40da2d24625aa81f69a63b5d51548f1b
wqp99w <wqp99w@naver.com>
2025-01-10 17:44:15 +0900

    테스트 삭제제

--- src/main/java/com/example/spring/repository/PosterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring.repository;
-
-import com.example.spring.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PosterRepository extends JpaRepository<Movie, Long> {
-
-}
